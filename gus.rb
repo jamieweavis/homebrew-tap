ec2e5b7be22248f056e5a841664d0004b8d67f99
Jamie <jamie.weavis@gmail.com>
2025-04-14 15:26:18 +0100

    gus v1.0.1

--- gus.rb
+++ gus.rb
@@ -1,12 +1,9 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Gus < Formula
-  desc "🐻 Git user switcher for quickly switching between local git users"
+  desc "Git User Switcher - Simple command line utility for quickly/easily switching between local git users" 
   homepage "https://github.com/jamieweavis/gus"
-  url "https://github.com/jamieweavis/gus/releases/download/v1.0.0/gus-mac.tar.gz"
-  sha256 "be2d70b4b94f322d738bd6d74251a06db14ff65bf80ed133ec34c134783209a1"
-  version "1.0.0"
+  url "https://github.com/jamieweavis/gus/releases/download/v1.0.1/gus-mac.tar.gz"
+  sha256 "98124fffa643eabba239ba64f5ad645f494ad430ea7da162e6c75610101a7de3" 
+  version "1.0.1"
 
   def install
     bin.install "gus"
