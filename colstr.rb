fee59a91b0f55e33791a5a53d044bbe7e583c431
Jamie <jamie.weavis@gmail.com>
2025-04-16 15:01:58 +0100

    colstr v1.0.0

--- colstr.rb
+++ colstr.rb
@@ -2,7 +2,7 @@ class Colstr < Formula
   desc "Simple command line utility to colorize strings based on delimiters contained within them" 
   homepage "https://github.com/jamieweavis/colstr"
   url "https://github.com/jamieweavis/colstr/releases/download/v1.0.0/colstr-mac.tar.gz"
-  sha256 "b8f8d08a7a0f0f8d006b789cbfedd35819ff71857a1ee159b26d65ce16203d09"
+  sha256 "60619b6e7a7be4d82ee2bd88e2e6815919a1b3847b2d31b8203b04872c2690cb"
   version "1.0.0"
 
   def install
