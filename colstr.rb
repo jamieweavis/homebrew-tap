72181b540feb0805e36aecd55154aa831d2160fd
Jamie Weavis <8133259+jamieweavis@users.noreply.github.com>
2025-10-28 18:26:15 +0000

    Fix colstr sha sum

--- colstr.rb
+++ colstr.rb
@@ -1,8 +1,8 @@
 class Colstr < Formula
-  desc "Simple command line utility to colorize strings based on delimiters contained within them" 
+  desc "Simple command line utility to colorize strings based on delimiters contained within them"
   homepage "https://github.com/jamieweavis/colstr"
   url "https://github.com/jamieweavis/colstr/releases/download/v1.0.0/colstr-mac.tar.gz"
-  sha256 "60619b6e7a7be4d82ee2bd88e2e6815919a1b3847b2d31b8203b04872c2690cb"
+  sha256 "95514193b39109576d8580f447f89a0c593bf71f09198e14b852f9aae6f63f5a"
   version "1.0.0"
 
   def install
